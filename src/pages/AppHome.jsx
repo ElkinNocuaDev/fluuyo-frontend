00e852a9f7417be98c6fe3027b2c8e7a120375c3
ElkinNocuaDev <startwavesacademy@gmail.com>
2026-02-12 21:07:19 -0500

    Ajuste vista reenvio email otp mod1-9-9-4-1

--- src/pages/AppHome.jsx
+++ src/pages/AppHome.jsx
@@ -4,8 +4,7 @@ import { Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "../auth/AuthProvider";
 import { routeByRole } from "../auth/routeByRole";
 import { apiFetch } from "../lib/api";
-// import logo from '../assets/fluuyo-logo-web-outlines.svg';
-import Logo from "../components/Logo";
+import logo from '../assets/fluuyo-logo-web-outlines.svg';
 
 import {
   LogOut,
@@ -205,7 +204,12 @@ export default function AppHome() {
           <div className="flex items-center gap-3">
             <div className="h-10 w-10 rounded-2xl bg-white/10 border border-white/10 backdrop-blur-xl flex items-center justify-center">
               <span className="text-lg font-black tracking-tight text-white">
-                <Logo className="h-10 w-auto" />
+                <img
+                                  src={logo}
+                                  alt="Fluuyo"
+                                  className="h-10 w-auto"
+                                  loading="eager"
+                                />
               </span>
             </div>
 
